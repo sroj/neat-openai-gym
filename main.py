d571e686c8286837450005c7079d5b86c56242d0
Simon Rojas <srojas@mahisoft.com>
2017-11-14 21:49:26 -0500

    Add informational prints

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import gym
 def main():
     env = gym.make('CartPole-v0')
     for i_episode in range(20):
+        print("--> Starting new episode")
         observation = env.reset()
         for t in range(100):
             env.render()
-            print(observation)
+            print("\t Observation {}: {}".format(t, observation))
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
             if done:
-                print("Episode finished after {} timesteps".format(t + 1))
+                print("<-- Episode finished after {} timesteps".format(t + 1))
                 break
 
 
