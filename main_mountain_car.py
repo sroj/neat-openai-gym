5261139c1760b8cb0fca2537def9a8b647004261
Simon Rojas <srojas@mahisoft.com>
2017-11-17 11:17:38 -0500

    Solve mountain car

--- main_mountain_car.py
+++ main_mountain_car.py
@@ -16,41 +16,45 @@ p.add_reporter(neat.StdOutReporter(False))
 
 env = gym.make('MountainCarContinuous-v0')
 
+n = 100
+t_steps = 5000
+
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
-        genome.fitness = 4.0
+        genome.fitness = 1.0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
-        for i_episode in range(20):
+        for i_episode in range(1):
             # print("--> Starting new episode")
             observation = env.reset()
 
             action = eval_network(net, observation)
 
-            for t in range(4000):
+            for t in range(t_steps):
 
                 # env.render()
 
                 observation, reward, done, info = env.step(action)
 
                 # print("\t Reward {}: {}".format(t, reward))
+                # print("\t Action {}: {}".format(t, action))
 
                 action = eval_network(net, observation)
 
                 genome.fitness += reward
 
                 if done:
-                    # print("<-- Episode finished after {} timesteps".format(t + 1))
+                    # print("<-- Episode finished after {} timesteps with reward {}".format(t + 1, genome.fitness))
                     break
 
 
 def eval_network(net, net_input):
-    assert (len(net_input == 4))
+    assert (len(net_input == 2))
 
-    result = np.argmax(net.activate(net_input))
+    result = net.activate(net_input)
 
-    assert (result == 0 or result == 1)
+    assert (result[0] >= -1.0 or result[0] <= 1.0)
 
     return result
 
@@ -76,7 +80,7 @@ def run_neat():
 
         reward_episode = 0
 
-        for t in range(500):
+        for t in range(t_steps):
 
             env.render()
 
@@ -88,6 +92,8 @@ def run_neat():
             action = eval_network(net, observation)
 
             reward_episode += reward
+
+            # print("\t Reward {}: {}".format(t, reward))
 
             if done:
                 print("<-- Test episode done after {} time steps with reward {}".format(t + 1, reward_episode))
