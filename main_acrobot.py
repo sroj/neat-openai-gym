a9dbdc0849bdc947a324e5f296b549599da17b75
Simon Rojas <srojas@mahisoft.com>
2017-12-12 21:54:12 -0500

    Add acrobot and cart pole v0

---
+++ main_acrobot.py
@@ -0,0 +1,55 @@
+import neat
+import numpy as np
+
+import run_neat_base
+
+
+def eval_single_genome(genome, genome_config):
+    net = neat.nn.FeedForwardNetwork.create(genome, genome_config)
+    total_reward = 0.0
+
+    for i in range(run_neat_base.n):
+        # print("--> Starting new episode")
+        observation = run_neat_base.env.reset()
+
+        action = eval_network(net, observation)
+
+        done = False
+
+        while not done:
+
+            # env.render()
+
+            observation, reward, done, info = run_neat_base.env.step(action)
+
+            # print("\t Reward {}: {}".format(t, reward))
+
+            action = eval_network(net, observation)
+
+            total_reward += reward
+
+            if done:
+                # print("<-- Episode finished after {} timesteps".format(t + 1))
+                break
+
+    return total_reward / run_neat_base.n
+
+
+def eval_network(net, net_input):
+    assert (len(net_input == 4))
+
+    result = np.argmax(net.activate(net_input))
+
+    assert (result in range(0, 3))
+
+    return result
+
+
+def main():
+    run_neat_base.run(eval_network,
+                      eval_single_genome,
+                      environment_name="Acrobot-v1")
+
+
+if __name__ == '__main__':
+    main()
